c5c4dbaa3916c6d395d5c314a4b0d8df8c0a58f9
GordonCwj <sishenjia@hotmail.com>
2021-06-09 10:40:22 +0800

    add two files

---
+++ EclipseWorkSpace/gordTwoTh/src/test/java/gordTwoTh/ShengMing.java
@@ -0,0 +1,9 @@
+package gordTwoTh;
+/*
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;*/
+
+public interface ShengMing {
+	//public static boolean isJudgingElement(WebDriver web); 
+	public void eat();
+}
---
+++ EclipseWorkSpace/gordTwoTh/src/test/java/gordTwoTh/ShiXian.java
@@ -0,0 +1,8 @@
+package gordTwoTh;
+
+public class ShiXian implements ShengMing {
+	public void eat()
+	{
+		System.out.println("I'm eating");
+	}
+}
